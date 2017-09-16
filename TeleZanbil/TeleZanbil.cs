ef18688202fc76ecd566583ce393c90f91e2c072
EmIT <Emad.Armoun@gmail.com>
2017-09-16 08:51:43 +0430

    ساخت کلاس های اولیه

---
+++ TeleZanbil/TeleZanbil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using ir.EmIT.EmITBotNet;
+using Telegram.Bot.Types;
+
+namespace TeleZanbil
+{
+    class TeleZanbil : EmITBotNetBase
+    {
+        public override void addNewUserSession(long currentUserID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Message convertData(Message m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void defineNFARulePostFunctions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void defineNFARules()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<long> getAuthenticatedUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void getConvertedSessionData(Message m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void initDatabase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
